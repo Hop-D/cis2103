c3b0789bfd1b8f302c1da9e318033456b28054c5
Hop-D <95290625+Hop-D@users.noreply.github.com>
2023-03-31 23:12:44 +0800

    Add files via upload

---
+++ cis2103/src/cis2103/AdminUser.java
@@ -0,0 +1,144 @@
+
+package cis2103;
+
+/**
+ *
+ * @author Janica Nyle Pino
+ */
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class AdminUser {
+    
+    Connection con = MyConnection.getConnection();
+    PreparedStatement ps;
+    
+    public int getMax() {
+        int id = 0;
+        Statement st;
+        try {
+            st = con.createStatement();
+            ResultSet rs = st.executeQuery("SELECT MAX(userID) FROM users");
+            while(rs.next()) {
+                id = rs.getInt(1);
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(AdminUser.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return id + 1;   
+    }
+    
+    public void getUsers(JTable table, String searchVal) {
+        String sql = "SELECT * FROM users WHERE CONCAT(userID, username, password, contact, usertype, userCreated, userUpdated)like ? ORDER BY userID DESC";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, "%" + searchVal + "%");
+            ResultSet rs = ps.executeQuery();
+            DefaultTableModel model = (DefaultTableModel) table.getModel();
+            Object[] row;
+            while(rs.next()) {
+                row = new Object[7];
+                row[0] = rs.getInt(1);
+                row[1] = rs.getString(2);
+                row[2] = rs.getString(3);
+                row[3] = rs.getString(4);
+                row[4] = rs.getString(5);
+                row[5] = rs.getString(6);
+                row[6] = rs.getString(7);
+                model.addRow(row);
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(AdminUser.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public boolean isUserExist(String username) {
+        String sql = "SELECT * FROM users where username = ?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, username);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()) {
+                return true;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(AdminUser.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+    
+    public void insertUser(int userID, String userName, String userPass, String userContact, String userType) {
+        String sql = "INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?)";
+        
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, userID);
+            ps.setString(2, userName);
+            ps.setString(3, userPass);
+            ps.setString(4, userContact);
+            ps.setString(5, userType);
+            ps.setTimestamp(6, new java.sql.Timestamp(new java.util.Date().getTime()));
+            ps.setTimestamp(7, new java.sql.Timestamp(new java.util.Date().getTime()));
+            
+            
+            if(ps.executeUpdate() > 0) {
+                JOptionPane.showMessageDialog(null, "New User Added Successfully");
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(AdminUser.class.getName()).log(Level.SEVERE, null, ex);
+        }  
+    }
+    
+    public void updateUser(int userID, String userName, String userPass, String userContact, String userType) {
+        String sql = "UPDATE users SET username = ?, password = ?, contact = ?, usertype = ?, userUpdated = ? WHERE userID = ?";
+        
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, userName);
+            ps.setString(2, userPass);
+            ps.setString(3, userContact);
+            ps.setString(4, userType);
+            ps.setTimestamp(5, new java.sql.Timestamp(new java.util.Date().getTime()));
+            ps.setInt(6, userID);
+            
+            if(ps.executeUpdate() > 0) {
+                JOptionPane.showMessageDialog(null, "User Updated Successfully");
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(AdminUser.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void removeUser(int userID) {
+        int choice = JOptionPane.showConfirmDialog(null, "Removing a user is irreversible", "User Remove", JOptionPane.OK_CANCEL_OPTION, 0);
+        
+        if(choice == JOptionPane.OK_OPTION) {
+            String sql = "DELETE FROM users WHERE userID = ?";
+            try {
+                ps = con.prepareStatement(sql);
+                ps.setInt(1, userID);
+                
+                if(ps.executeUpdate() > 0) {
+                JOptionPane.showMessageDialog(null, "User Removed Successfully");
+            }
+                
+            } catch (SQLException ex) {
+                Logger.getLogger(AdminUser.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    
+    
+
+}
---
+++ db/cis2103.sql
@@ -0,0 +1,201 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 31, 2023 at 04:44 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cis2103`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `itemID` int(11) NOT NULL,
+  `itemName` varchar(50) NOT NULL,
+  `itemPrice` int(11) NOT NULL,
+  `itemCount` int(11) NOT NULL,
+  `itemAdded` timestamp NOT NULL DEFAULT current_timestamp(),
+  `itemUpdated` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`itemID`, `itemName`, `itemPrice`, `itemCount`, `itemAdded`, `itemUpdated`) VALUES
+(1, 'Soup', 200, 0, '2023-03-29 03:31:19', '2023-03-29 05:06:19'),
+(2, 'Fried Chicken', 200, 0, '2023-03-29 03:31:19', '2023-03-29 05:06:31'),
+(3, 'Choco Milkshake', 200, 0, '2023-03-29 03:31:19', '2023-03-29 05:10:21'),
+(4, 'Coke', 50, 0, '2023-03-29 03:31:19', '2023-03-29 03:31:19'),
+(5, 'Sisig', 150, 0, '2023-03-29 03:35:07', '2023-03-29 03:35:07'),
+(6, 'Pinakbet', 80, 0, '2023-03-29 03:41:44', '2023-03-29 08:05:56'),
+(7, 'Coffee', 100, 0, '2023-03-29 03:43:24', '2023-03-29 03:43:24'),
+(8, 'French Fries', 50, 0, '2023-03-29 03:49:14', '2023-03-29 03:49:14'),
+(9, 'Burger', 90, 0, '2023-03-29 04:10:18', '2023-03-29 08:05:23'),
+(10, 'French Toast', 80, 0, '2023-03-29 04:10:47', '2023-03-29 08:05:51'),
+(11, 'Pancake', 150, 0, '2023-03-29 04:14:14', '2023-03-29 08:09:42'),
+(12, 'Spaghetti', 200, 0, '2023-03-29 04:15:20', '2023-03-29 04:15:20'),
+(13, 'Coffee Jelly', 101, 0, '2023-03-29 10:57:56', '2023-03-29 13:07:21');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `packages`
+--
+
+CREATE TABLE `packages` (
+  `packageID` int(11) NOT NULL,
+  `packageName` varchar(50) NOT NULL,
+  `packagePrice` varchar(50) NOT NULL,
+  `itemCount` int(11) NOT NULL,
+  `packageCount` int(11) NOT NULL,
+  `packageCreated` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `packages`
+--
+
+INSERT INTO `packages` (`packageID`, `packageName`, `packagePrice`, `itemCount`, `packageCount`, `packageCreated`) VALUES
+(1, 'Package 1', '500', 3, 0, '2023-03-29 13:53:56'),
+(2, 'Package 2', '100', 3, 0, '2023-03-29 14:46:23');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `packitem`
+--
+
+CREATE TABLE `packitem` (
+  `packItemID` int(11) NOT NULL,
+  `itemID` int(11) NOT NULL,
+  `packageID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `packitem`
+--
+
+INSERT INTO `packitem` (`packItemID`, `itemID`, `packageID`) VALUES
+(2, 5, 1),
+(3, 2, 2),
+(4, 2, 1),
+(5, 4, 2),
+(6, 1, 2),
+(7, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `userID` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `contact` varchar(11) NOT NULL,
+  `usertype` varchar(10) NOT NULL,
+  `userCreated` timestamp NOT NULL DEFAULT current_timestamp(),
+  `userUpdated` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userID`, `username`, `password`, `contact`, `usertype`, `userCreated`, `userUpdated`) VALUES
+(1, 'janica', 'password', '09232576116', 'admin', '2023-03-29 11:15:36', '2023-03-29 11:15:36'),
+(2, 'jempai', 'password', '09232576116', 'regular', '2023-03-29 12:51:08', '2023-03-29 13:08:34');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`itemID`);
+
+--
+-- Indexes for table `packages`
+--
+ALTER TABLE `packages`
+  ADD PRIMARY KEY (`packageID`);
+
+--
+-- Indexes for table `packitem`
+--
+ALTER TABLE `packitem`
+  ADD PRIMARY KEY (`packItemID`),
+  ADD KEY `itemID` (`itemID`),
+  ADD KEY `packageID` (`packageID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `itemID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `packages`
+--
+ALTER TABLE `packages`
+  MODIFY `packageID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `packitem`
+--
+ALTER TABLE `packitem`
+  MODIFY `packItemID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `packitem`
+--
+ALTER TABLE `packitem`
+  ADD CONSTRAINT `packitem_ibfk_1` FOREIGN KEY (`itemID`) REFERENCES `items` (`itemID`),
+  ADD CONSTRAINT `packitem_ibfk_2` FOREIGN KEY (`packageID`) REFERENCES `packages` (`packageID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
