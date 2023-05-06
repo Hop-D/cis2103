4a91930a7a1458766180576f0337ac52525a3754
kueueueue <111956592+kueueueue@users.noreply.github.com>
2023-05-06 23:47:39 +0800

    sadf

--- pino/cis2103/src/model/Database.java
+++ pino/cis2103/src/model/Database.java
@@ -518,49 +518,7 @@ public class Database {
 	    }
 	}
 	
-	public static void updatePackage(String id, String name, float price, ArrayList<Item> packageItems) throws SQLException {
-		Database db = new Database();
-		try {
-			db = new Database();
-			db.setPst("UPDATE package SET name = ?, price = ?, dateUpdated = current_timestamp() WHERE packageID = ?");
-			db.getPst().setString(1, name);
-			db.getPst().setFloat(2, price);
-			db.getPst().executeUpdate();
-			
-			for(Item item: items) {
-				if(id.equals(item.getId())) {
-					item.setName(name);
-					item.setPrice(price);
-					db.getPst().setString(3, id);
-					item.setDateUpdated(LocalDateTime.now());
-					return;
-				}
-			}
-	    }finally {
-	        if (db != null) {
-	            db.closeConn();
-	        }
-	    }
-	}
 	
-	public static void  removeMenu(Package packag) throws SQLException {
-		Database db = new Database();
-		try {
-			db = new Database();
-			db.setPst("DELETE FROM menu WHERE menuID = (SELECT menuID FROM package WHERE packageID = ?)");
-			db.getPst().setString(1, packag.getId());
-			db.getPst().executeUpdate();
-			pack.remove(packag);
-			menu.remove(packag);
-			
-	    } finally {
-	        if (db != null) {
-	            db.closeConn();
-	        }
-	    }
-		
-		
-	}
 	
 	public static void updatePackage(String id, String name, float price, ArrayList<Item> packageItems) throws SQLException {
 		Database db = new Database();
