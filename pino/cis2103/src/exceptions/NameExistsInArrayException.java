3534499ad0a377a9e5dbe56d5e6c2a38110a3396
Hop-D <95290625+Hop-D@users.noreply.github.com>
2023-05-08 12:55:34 +0800

    Add files via upload

---
+++ pino/cis2103/src/exceptions/NameExistsInArrayException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class NameExistsInArrayException extends Exception {
+	public NameExistsInArrayException(String s) {
+		super(s);
+	}
+	public NameExistsInArrayException() {
+		super("Name already exists.");
+	}
+}
